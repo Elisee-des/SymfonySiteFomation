54292b55a10f39cc0259e63c21cc69fe0d060c86
Elisee-des <esabidani@gmail.com>
2022-06-24 01:42:10 +0000

    Separation des controllers

---
+++ src/Controller/User/ContactController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller\User;
+
+use App\Form\ContactType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/utilisateur/contact", name="utilisateur_contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('user/contact/contact.html.twig', []);
+    }
+
+    /**
+     * @Route("/utilisateur/contact/email", name="utilisateur_contact_email")
+     */
+    public function contactEmail(Request $request, MailerInterface $mailer): Response
+    {
+        $form = $this->createForm(ContactType::class);
+
+        $contact = $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $email = (new TemplatedEmail())
+                ->from($contact->get('email')->getData())
+                ->to("mondomain@gmail.com")
+                ->subject("Test Mail")
+                ->htmlTemplate("user/contact/emailModel.html.twig")
+                ->context([
+                    'mail' => $contact->get("email")->getData(),
+                    'sujet' => $contact->get("sujet")->getData(),
+                    'message' => $contact->get("message")->getData()
+                ]);
+
+            $mailer->send($email);
+        }
+
+        return $this->render('user/contact/email.html.twig', [
+            "form" => $form->createView()
+        ]);
+    }
+
+}
