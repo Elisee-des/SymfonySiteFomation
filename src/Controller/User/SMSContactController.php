51de99329a947ad64ac0eb0e01a8100f3c658e7d
Elisee-des <esabidani@gmail.com>
2022-06-29 20:55:34 +0000

    Gestion avec succes de l'envoie des SMS

--- src/Controller/User/SMSContactController.php
+++ src/Controller/User/SMSContactController.php
@@ -28,6 +28,43 @@ class SMSContactController extends AbstractController
          */
         $form = $this->createForm(SMSContactType::class);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $message = $request->get('sms_contact')['message'];
+            $numeroDestinateur = $request->get('sms_contact')['numero'];
+
+            $url="https://www.aqilas.com/api/v1/sms";
+
+            $data=[
+                "from"=>"Sabidani",
+                "to"=>[$numeroDestinateur],
+                "text"=>$message
+            ];
+            $dataJson = json_encode($data);
+            $header=[
+                'Content-Type: application/json',
+                "X-AUTH-TOKEN: bca6852f-1703-4417-bac4-411712e3e5b0"
+            ];
+            
+            $apiUrl=curl_init($url);
+            curl_setopt($apiUrl,CURLOPT_HTTPHEADER,$header);
+            curl_setopt($apiUrl,CURLOPT_POSTFIELDS,$dataJson);
+            curl_setopt($apiUrl,CURLOPT_RETURNTRANSFER,true);
+    
+            curl_exec($apiUrl);
+    
+            curl_close($apiUrl);
+
+            $this->addFlash(
+               'message',
+               'Votre sms a ete envoyez avec success. Nous vous contacterons sous peu'
+            );
+
+            return $this->redirectToRoute('utilisateur_contact_sms');
+        }
+
         return $this->render('user/contact/sms.html.twig', [
             "form" => $form->createView()
         ]);
--- src/Form/SMSContactType.php
+++ src/Form/SMSContactType.php
@@ -6,6 +6,7 @@ use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,17 +16,19 @@ class SMSContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('numero', NumberType::class, [
-            'label'=>'Numero destinateur'
-        ])
-            ->add('titre', TextType::class, [
-                'label'=>'Titre'
+            ->add('numero', null, [
+                'label' => 'Numero du destinateur preceder +226',
+                'attr'=>[
+                    'placeholder'=>'Exemple: +22656885237'
+                ]
+            ])  
+            ->add('message', TextareaType::class, [
+                'label' => 'Message',
+                'attr'=>[
+                    'placeholder'=>'Saisissez la votre message'
+                ]
             ])
-            ->add('description', CKEditorType::class, [
-                'label'=>'Description'
-            ])
-            ->add('Envoyez', SubmitType::class)
-        ;
+            ->add('Envoyez', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
